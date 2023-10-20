7ea7a4d9be1eca0fc5d67b3ed846fb54185b7910
River <riverdejesus2@gmail.com>
2023-10-20 12:45:35 -0400

    Update fix missing files

---
+++ src/main/java/com/example/studychat/StudyChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.studychat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studychat/StudyChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studychat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
