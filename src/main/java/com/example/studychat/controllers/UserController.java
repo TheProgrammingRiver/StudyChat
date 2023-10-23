87a617477e1f7c1416e8da965d83769390a580a5
River <riverdejesus2@gmail.com>
2023-10-23 16:33:46 -0400

    add @GetMapping getUserByUsername

--- src/main/java/com/example/studychat/controllers/UserController.java
+++ src/main/java/com/example/studychat/controllers/UserController.java
@@ -5,10 +5,9 @@ import com.example.studychat.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 
 @RestController
@@ -22,6 +21,12 @@ public class UserController {
         private UserService userService;
 
 
+        @GetMapping("/username/{username}")
+        public ResponseEntity<User> getUserByUsername(@PathVariable String username) {
+            Optional<User> user = userService.findByUsername(username);
+            return user.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                    .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        }
 
         @PostMapping("/register")
         public ResponseEntity<String> register(@RequestBody User user) {
