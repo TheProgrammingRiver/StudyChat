fe6f7a8f5136d51ec89c645444e75e4827bb8dba
River <riverdejesus2@gmail.com>
2023-10-23 12:44:12 -0400

    Added ChatMessage class with getters, setters, and constructors

--- src/main/java/com/example/studychat/models/ChatMessage.java
+++ src/main/java/com/example/studychat/models/ChatMessage.java
@@ -4,6 +4,18 @@ import java.sql.Timestamp;
 
 public class ChatMessage {
 
+    private Long messageId;
+    private String text;
+    private Timestamp timestamp;
+
+
+    private User sender;
+
+
+    private StudyRoom studyRoom;
+
+    public ChatMessage() { }
+
     public ChatMessage(String text, Timestamp timestamp, User sender, StudyRoom studyRoom) {
         this.text = text;
         this.timestamp = timestamp;
@@ -11,9 +23,35 @@ public class ChatMessage {
         this.studyRoom = studyRoom;
     }
 
+    public Long getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(Long messageId) {
+        this.messageId = messageId;
+    }
+
     public String getText() {
         return text;
     }
-    
 
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public StudyRoom getStudyRoom() {
+        return studyRoom;
+    }
+
+    public void setStudyRoom(StudyRoom studyRoom) {
+        this.studyRoom = studyRoom;
+    }
 }
--- src/main/java/com/example/studychat/models/User.java
+++ src/main/java/com/example/studychat/models/User.java
@@ -1,4 +1,5 @@
 package com.example.studychat.models;
 
+
 public class User {
 }
