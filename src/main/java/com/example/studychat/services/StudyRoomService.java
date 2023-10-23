1f3a9c9baf135fdb194d625cf9077acd230587e6
River <riverdejesus2@gmail.com>
2023-10-23 14:55:19 -0400

    update make createRoom

--- src/main/java/com/example/studychat/services/StudyRoomService.java
+++ src/main/java/com/example/studychat/services/StudyRoomService.java
@@ -1,13 +1,35 @@
 package com.example.studychat.services;
 
+import com.example.studychat.models.StudyRoom;
+import com.example.studychat.repositores.StudyRoomRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.Optional;
 
+
+@Service
 public class StudyRoomService {
-    public Optional<Object> findById(Long roomId) {
-        return Optional.empty();
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(StudyRoomService.class);
+
+
+@Autowired
+private StudyRoomRepository studyRoomRepository;
+
+
+public StudyRoom createRoom(StudyRoom studyRoom) {
+        LOGGER.info("Creating study room with name: {}", studyRoom.getName());
+        return studyRoomRepository.save(studyRoom);
+        }
+
+    public Optional<StudyRoom> findById(Long roomId) {
+        LOGGER.info("Fetching study room by ID: {}", roomId);
+        return studyRoomRepository.findById(roomId);
     }
 
-    public boolean existsById(Long roomId) {
-        return false;
-    }
+    public boolean existsById(Long roomId) { return studyRoomRepository.existsById(roomId);}
 }
+
