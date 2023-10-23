19082dbed1f99478acf05adc20bafba4610a9c9a
River <riverdejesus2@gmail.com>
2023-10-23 14:13:58 -0400

    Update ChatMessageService.java

--- src/main/java/com/example/studychat/services/ChatMessageService.java
+++ src/main/java/com/example/studychat/services/ChatMessageService.java
@@ -1,4 +1,10 @@
 package com.example.studychat.services;
 
+import com.example.studychat.models.ChatMessage;
+
 public class ChatMessageService {
+
+    public ChatMessage sendMessage(ChatMessage message) {
+        return message;
+    }
 }
