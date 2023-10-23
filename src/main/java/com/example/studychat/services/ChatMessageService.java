36fc12017ee60f53c61a8a27fd70c45cd2981896
River <riverdejesus2@gmail.com>
2023-10-23 14:27:56 -0400

    sendMessage ChatMessageService

--- src/main/java/com/example/studychat/services/ChatMessageService.java
+++ src/main/java/com/example/studychat/services/ChatMessageService.java
@@ -1,10 +1,30 @@
 package com.example.studychat.services;
 
+import com.example.studychat.exceptions.StudyChatException;
 import com.example.studychat.models.ChatMessage;
+import com.example.studychat.models.StudyRoom;
+import com.example.studychat.repositores.ChatMessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class ChatMessageService {
 
-    public ChatMessage sendMessage(ChatMessage message) {
-        return message;
+    @Autowired
+    private ChatMessageRepository chatMessageRepository;
+
+    @Autowired
+    private StudyRoomService studyRoomService;
+
+    public ChatMessage sendMessage(ChatMessage message) throws StudyChatException {
+        if (message == null) {
+            throw new StudyChatException("Message cannot be null.");
+        }
+
+        StudyRoom room = message.getStudyRoom();
+        if (room == null || !studyRoomService.existsById(room.getRoomId())) {
+            throw new StudyChatException("Study room does not exist.");
+        }
+
+        return chatMessageRepository.save(message);
     }
+
 }
