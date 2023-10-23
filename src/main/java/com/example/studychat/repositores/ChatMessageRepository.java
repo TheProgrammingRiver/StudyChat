1efd152305ef372ae9408efffc53bf524d9c32e5
River <riverdejesus2@gmail.com>
2023-10-23 14:37:59 -0400

    Update ChatMessageRepository.java

--- src/main/java/com/example/studychat/repositores/ChatMessageRepository.java
+++ src/main/java/com/example/studychat/repositores/ChatMessageRepository.java
@@ -1,7 +1,13 @@
 package com.example.studychat.repositores;
 
 import com.example.studychat.models.ChatMessage;
+import com.example.studychat.models.StudyRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChatMessageRepository {
+import java.util.List;
+
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
     ChatMessage save(ChatMessage message);
+
+    List<ChatMessage> findByStudyRoom(StudyRoom studyRoom);
 }
