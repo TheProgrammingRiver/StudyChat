447320100e068ec0bb08735ec016ee0ddb2078d2
River <riverdejesus2@gmail.com>
2023-10-23 14:13:48 -0400

    added @PostMapping to ChatMessageController

--- src/main/java/com/example/studychat/controllers/ChatMessageController.java
+++ src/main/java/com/example/studychat/controllers/ChatMessageController.java
@@ -1,4 +1,32 @@
 package com.example.studychat.controllers;
 
+import com.example.studychat.models.ChatMessage;
+import com.example.studychat.models.StudyRoom;
+import com.example.studychat.services.ChatMessageService;
+import com.example.studychat.services.StudyRoomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/studyroom/{roomId}/messages")
 public class ChatMessageController {
+
+    @Autowired
+    private ChatMessageService chatMessageService;
+
+    @Autowired
+    private StudyRoomService studyRoomService;
+
+    @PostMapping("/send")
+    public ResponseEntity<ChatMessage> sendMessage(@PathVariable Long roomId, @RequestBody ChatMessage message) {
+        StudyRoom room = studyRoomService.findById(roomId).orElse(null);
+        if (room == null) {
+            return ResponseEntity.notFound().build();
+        }
+        message.setStudyRoom(room); // Setting the room in the message
+        ChatMessage sentMessage = chatMessageService.sendMessage(message);
+        return ResponseEntity.ok(sentMessage);
+    }
 }
