16b1380c3f916c2a32ee13f60eb6d44b07a99ad3
River <riverdejesus2@gmail.com>
2023-10-23 14:27:17 -0400

    Update ChatMessageRepository.java

--- src/main/java/com/example/studychat/repositores/ChatMessageRepository.java
+++ src/main/java/com/example/studychat/repositores/ChatMessageRepository.java
@@ -1,4 +1,7 @@
 package com.example.studychat.repositores;
 
+import com.example.studychat.models.ChatMessage;
+
 public interface ChatMessageRepository {
+    ChatMessage save(ChatMessage message);
 }
