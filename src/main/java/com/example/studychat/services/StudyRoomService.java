afa74b97246b50213dce4121a2ddb73ad7bdc9b0
River <riverdejesus2@gmail.com>
2023-10-23 14:13:55 -0400

    Update StudyRoomService.java

--- src/main/java/com/example/studychat/services/StudyRoomService.java
+++ src/main/java/com/example/studychat/services/StudyRoomService.java
@@ -1,4 +1,9 @@
 package com.example.studychat.services;
 
+import java.util.Optional;
+
 public class StudyRoomService {
+    public Optional<Object> findById(Long roomId) {
+        return Optional.empty();
+    }
 }
