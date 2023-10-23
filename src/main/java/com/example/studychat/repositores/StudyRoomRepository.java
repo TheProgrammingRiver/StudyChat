c02b548690ffd5015059e828a5d3f7bc603b374d
River <riverdejesus2@gmail.com>
2023-10-23 15:27:36 -0400

    Update StudyRoomRepository.java

--- src/main/java/com/example/studychat/repositores/StudyRoomRepository.java
+++ src/main/java/com/example/studychat/repositores/StudyRoomRepository.java
@@ -2,9 +2,14 @@ package com.example.studychat.repositores;
 
 import com.example.studychat.models.StudyRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
 
-
+@Repository
 public interface StudyRoomRepository extends JpaRepository<StudyRoom, Long> {
-
+    @Query("SELECT sr FROM StudyRoom sr JOIN sr.users u WHERE u.userId = :userId")
+    List<StudyRoom> findRoomsByUserId(@Param("userId") Long userId);
 }
