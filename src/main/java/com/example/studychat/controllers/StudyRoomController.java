9fa1db810abfc44b0860a36a7e28eafbbabd2cdb
River <riverdejesus2@gmail.com>
2023-10-23 14:57:41 -0400

    Update @PostMapping @GetMapping

--- src/main/java/com/example/studychat/controllers/StudyRoomController.java
+++ src/main/java/com/example/studychat/controllers/StudyRoomController.java
@@ -1,4 +1,39 @@
 package com.example.studychat.controllers;
 
+import com.example.studychat.exceptions.StudyChatException;
+import com.example.studychat.models.StudyRoom;
+import com.example.studychat.models.User;
+import com.example.studychat.services.StudyRoomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/studyrooms")
 public class StudyRoomController {
+
+    @Autowired
+    private StudyRoomService studyRoomService;
+
+    @PostMapping("/create")
+    public ResponseEntity<StudyRoom> createRoom(@RequestBody StudyRoom studyRoom) {
+        if (studyRoom == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        StudyRoom createdRoom = studyRoomService.createRoom(studyRoom);
+        return new ResponseEntity<>(createdRoom, HttpStatus.CREATED);
+    }
+
+
+
+    @GetMapping("/{roomId}")
+    public ResponseEntity<StudyRoom> getRoomDetails(@PathVariable Long roomId) {
+        StudyRoom room = studyRoomService.findById(roomId).orElseThrow(() -> new StudyChatException("Study room not found."));
+        return ResponseEntity.ok(room);
+    }
+
 }
+
