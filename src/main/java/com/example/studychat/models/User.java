3a3019d0ea900ac5de1b6ad8663c1492e9f30c9f
River <riverdejesus2@gmail.com>
2023-10-23 12:49:54 -0400

    Added User class with getters, setters, and isEmpty method

--- src/main/java/com/example/studychat/models/User.java
+++ src/main/java/com/example/studychat/models/User.java
@@ -1,5 +1,44 @@
 package com.example.studychat.models;
 
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class User {
+
+    private Long userId;
+    private String username;
+    private String password;
+
+
+    private Set<StudyRoom> studyRooms = new HashSet<>();
+
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEmpty() {
+        return studyRooms.isEmpty();
+    }
 }
