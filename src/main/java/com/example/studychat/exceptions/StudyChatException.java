fb4323d04d39588cb23b307c6c08826d21649f29
River <riverdejesus2@gmail.com>
2023-10-23 15:03:05 -0400

    Update StudyChatException.java

--- src/main/java/com/example/studychat/exceptions/StudyChatException.java
+++ src/main/java/com/example/studychat/exceptions/StudyChatException.java
@@ -1,6 +1,7 @@
 package com.example.studychat.exceptions;
 
 public class StudyChatException extends Throwable {
-    public StudyChatException(String s) {
+    public StudyChatException(String message) {
+    super(message);
     }
 }
