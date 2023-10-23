d196278c61a2025f51d94180ab7805d2c49fffdf
River <riverdejesus2@gmail.com>
2023-10-23 16:34:12 -0400

    add joinRoom

--- src/main/java/com/example/studychat/services/StudyRoomService.java
+++ src/main/java/com/example/studychat/services/StudyRoomService.java
@@ -1,6 +1,8 @@
 package com.example.studychat.services;
 
+import com.example.studychat.exceptions.StudyChatException;
 import com.example.studychat.models.StudyRoom;
+import com.example.studychat.models.User;
 import com.example.studychat.repositores.StudyRoomRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +21,9 @@ public class StudyRoomService {
 @Autowired
 private StudyRoomRepository studyRoomRepository;
 
+@Autowired
+private UserService userService;
+
 
 public StudyRoom createRoom(StudyRoom studyRoom) {
         LOGGER.info("Creating study room with name: {}", studyRoom.getName());
@@ -28,6 +33,20 @@ public StudyRoom createRoom(StudyRoom studyRoom) {
     public Optional<StudyRoom> findById(Long roomId) {
         LOGGER.info("Fetching study room by ID: {}", roomId);
         return studyRoomRepository.findById(roomId);
+    }
+
+    public StudyRoom joinRoom(Long roomId, User user) {
+        StudyRoom room = this.findById(roomId).orElseThrow(() -> new StudyChatException("Study room not found."));
+
+
+        User actualUser = userService.findByUsername(user.getUsername())
+                .orElseThrow(() -> new StudyChatException("User not found."));
+        if (room == null) {
+            throw new StudyChatException("Study room not found.");
+        }
+
+        room.getUsers().add(actualUser);
+        return studyRoomRepository.save(room);
     }
 
     public boolean existsById(Long roomId) { return studyRoomRepository.existsById(roomId);}
