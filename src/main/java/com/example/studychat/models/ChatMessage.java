9f85769dffdc58c7af0ddfe04d48b703ba052ad4
River <riverdejesus2@gmail.com>
2023-10-23 12:22:35 -0400

    Update ChatMessage.java

--- src/main/java/com/example/studychat/models/ChatMessage.java
+++ src/main/java/com/example/studychat/models/ChatMessage.java
@@ -1,4 +1,19 @@
 package com.example.studychat.models;
 
+import java.sql.Timestamp;
+
 public class ChatMessage {
+
+    public ChatMessage(String text, Timestamp timestamp, User sender, StudyRoom studyRoom) {
+        this.text = text;
+        this.timestamp = timestamp;
+        this.sender = sender;
+        this.studyRoom = studyRoom;
+    }
+
+    public String getText() {
+        return text;
+    }
+    
+
 }
