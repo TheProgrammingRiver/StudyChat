b9e17b9e595741b002819438cb336806802aa6e9
River <riverdejesus2@gmail.com>
2023-10-23 15:03:26 -0400

    added @ExceptionHandler StudyChatExceptionHandler

--- src/main/java/com/example/studychat/exceptions/StudyChatExceptionHandler.java
+++ src/main/java/com/example/studychat/exceptions/StudyChatExceptionHandler.java
@@ -1,4 +1,13 @@
 package com.example.studychat.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
 public class StudyChatExceptionHandler {
+
+    @ExceptionHandler(StudyChatException.class)
+    public ResponseEntity<String> handleStudyChatException(StudyChatException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 }
